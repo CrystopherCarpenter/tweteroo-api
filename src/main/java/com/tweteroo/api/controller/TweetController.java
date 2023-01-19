d9d39cf696ccf4517721c0b294e0782c9670cfa0
Crystopher Carpenter <crystophercarpenter@pop-os.localdomain>
2023-01-19 14:58:05 -0300

    Feat: add Post Tweet

--- src/main/java/com/tweteroo/api/controller/TweetController.java
+++ src/main/java/com/tweteroo/api/controller/TweetController.java
@@ -1,5 +1,29 @@
 package com.tweteroo.api.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tweteroo.api.dtos.TweetDTO;
+import com.tweteroo.api.models.Tweet;
+import com.tweteroo.api.repositories.TweetRepository;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/tweets")
 public class TweetController {
+
+    @Autowired
+    private TweetRepository repository;
+
+    @PostMapping
+    public String create(@RequestBody @Valid TweetDTO req) {
+        repository.save(new Tweet(req));
+
+        return ("OK");
+    }
 
 }
