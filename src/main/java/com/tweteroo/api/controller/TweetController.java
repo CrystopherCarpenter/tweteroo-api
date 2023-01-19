18131ef613391259c2399be9991ada6329496208
Crystopher Carpenter <crystophercarpenter@pop-os.localdomain>
2023-01-19 15:01:16 -0300

    Feat: add listAll method to tweetRepository with pagination

--- src/main/java/com/tweteroo/api/controller/TweetController.java
+++ src/main/java/com/tweteroo/api/controller/TweetController.java
@@ -1,9 +1,17 @@
 package com.tweteroo.api.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tweteroo.api.dtos.TweetDTO;
@@ -24,6 +32,14 @@ public class TweetController {
         repository.save(new Tweet(req));
 
         return ("OK");
+    }
+
+    @GetMapping
+    public List<Tweet> listAll(@RequestParam(name = "page", defaultValue = "1") int page) {
+        Pageable pagination = PageRequest.of(page, 5, Sort.by("id").descending());
+
+        Page<Tweet> tweets = repository.findAll(pagination);
+        return tweets.getContent();
     }
 
 }
