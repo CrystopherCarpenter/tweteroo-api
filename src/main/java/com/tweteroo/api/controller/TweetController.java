9801f48c2ee493803938b53dd1502948a103f9d6
Crystopher Carpenter <crystophercarpenter@pop-os.localdomain>
2023-01-19 15:09:17 -0300

    Feat: add listUserTweets method to tweetRepository with pagination

--- src/main/java/com/tweteroo/api/controller/TweetController.java
+++ src/main/java/com/tweteroo/api/controller/TweetController.java
@@ -8,6 +8,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -40,6 +41,15 @@ public class TweetController {
 
         Page<Tweet> tweets = repository.findAll(pagination);
         return tweets.getContent();
+    }
+
+    @GetMapping("/{username}")
+    public List<Tweet> listUserTweets(@PathVariable String username, @RequestBody @Valid TweetDTO req,
+            @RequestParam(name = "page", defaultValue = "1") int page) {
+        Pageable pagination = PageRequest.of(page, 5, Sort.by("id").descending());
+
+        List<Tweet> tweets = repository.findAllByUsername(username, pagination);
+        return tweets;
     }
 
 }
